5ba8adfc7c00210cba987db6fdf6b5b920441315
midddddiana24 <robertojrmediana@gmail.com>
2026-05-11 12:02:14 +0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ const allowedOrigins = [
   'http://localhost:8080',
   'http://127.0.0.1:8080',
   'https://shopco-ecommerce-three.vercel.app',
+  'https://shopco-ecommerce-pm93.onrender.com',
   'null'
 ].filter(Boolean);
 
